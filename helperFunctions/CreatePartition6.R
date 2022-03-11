1421455f16fbdc93704ebfcf9e634e86b6e13586
pottj <pott.ne.ja@gmail.com>
2022-03-11 18:34:07 +0100

    create partitions for n=6

--- helperFunctions/CreatePartition6.R
+++ helperFunctions/CreatePartition6.R
@@ -1,36 +1,65 @@
-#' What is my goal?
+#' ---
+#' title: "Creating all 4WPs for n=6 "
+#' subtitle: "Simulation Phylogenetic Decisivness"
+#' author: "Janne Pott"
+#' date: "11.03.2022"
+#' output:
+#'   html_document:
+#'     toc: true
+#'     number_sections: true
+#'     toc_float: true
+#'     code_folding: show
+#' ---
+#'
+#' # Intro ####
+#' ***
+#' I want to create a data table containing all possible four-way partitions with n=6 taxa. The data table should contain the following columns: 
 #' 
-#' I want a function that gives me a TRUE/FALSE reply pending on the four-way partition property. 
+#' * set_i: taxa included in partition i of 4
+#' * allQuads: all quadruples possible with the given partition, separated by "|"
 #' 
+#' According to Stirling numbers of the second kind, there are 65 different partitions for 6 taxa. 
 #' 
+#' There are two types of partitions: 
+#' 
+#' 1) a | b | c | def
+#' 2) a | b | cd | ef
+#' 
+#' I create them separately and combine them at the end. 
+#' 
+#' In addition, I test the table with the example data set 2 (phylo. dec.)
+#' 
+#' # Init ####
+#' ***
+rm(list = ls())
+time0<-Sys.time()
+
+source("myStirlingFunction.R")
+.libPaths()
 library(PhyloDecR)
 library(data.table)
 library(foreach)
 
-fn2 = system.file("extdata", 
-                  "example_2_Decisive.txt", 
-                  package = "PhyloDecR")
-test2 = createInput(fn=fn2,sepSym = ",")
-dummy = test2$data
-dummy[status=="input"]
-#' These are my input quadruples. If I understand the 4WPP & PD correctly, I must simply check, if all partitions of n=6 taxa are covered by these 11 quadruples. 
-#' 
-#' I need a quick way to generate all possible partitions
-#' 
-#' For n=6: a | b | cd | ef or a | b | c | def possible 
-#' 
+#' # Check & general parameters ####
+#' ***
+myStirlingFunction(n=6,k=4)
+
 n=6
 x = c(1:n)
 
+#' # First set of partitions: triples ####
+#' ***
+#' a | b | c | def  
+#' 
 allTriples = t(combn(n,3))
 allTriples<-data.table::as.data.table(allTriples)
-names(allTriples) = c("taxa1","taxa2","taxa3")
-allTriples[,triple := paste(taxa1,taxa2,taxa3,sep="_")]
+names(allTriples) = c("set1","set2","set3")
+allTriples[,triple := paste(set1,set2,set3,sep="_")]
 
 dumTab = foreach(i = 1:dim(allTriples)[1])%do%{
   #i=1
   myRow = allTriples[i,]
-  myX = c(myRow$taxa1,myRow$taxa2,myRow$taxa3)
+  myX = c(myRow$set1,myRow$set2,myRow$set3)
   notmyX = x[!is.element(x,myX)]
   
   quads = c()
@@ -45,27 +74,29 @@ dumTab = foreach(i = 1:dim(allTriples)[1])%do%{
   
   quads
   myQuads <-paste(quads, collapse = "|")
+  myRow[,set4 := paste(notmyX, collapse = "|")]
   myRow[,allQuads := myQuads]
+  myRow[,triple := NULL]
   myRow
   
 }
 
-dum2 = rbindlist(dumTab)
-dum2
+tab1 = rbindlist(dumTab)
+head(tab1)
 
-#' okay, dum2 contains all possibilities for triple + any other taxa (a | b | c | def = 3*20 possibilities)
+#' # Second set of partitions: tuples ####
+#' ***
+#' a | b | cd | ef
 #' 
-#' now the other option (a | b | cd | ef)
-
 allTuples = t(combn(n,2))
 allTuples<-data.table::as.data.table(allTuples)
-names(allTuples) = c("taxa1","taxa2")
-allTuples[,tuple := paste(taxa1,taxa2,sep="_")]
+names(allTuples) = c("set1","set2")
+allTuples[,tuple := paste(set1,set2,sep="_")]
 
 dumTab = foreach(i = 1:dim(allTuples)[1])%do%{
   #i=1
   myRow = allTuples[i,]
-  myX = c(myRow$taxa1,myRow$taxa2)
+  myX = c(myRow$set1,myRow$set2)
   notmyX = x[!is.element(x,myX)]
   
   a = t(combn(length(notmyX),2))
@@ -80,8 +111,8 @@ dumTab = foreach(i = 1:dim(allTuples)[1])%do%{
     x1 = notmyX[a2[1]]
     x2 = notmyX[a2[2]]
     x3 = notmyX[!is.element(notmyX,c(x1,x2))]
-    myRow2[,tuple2:=paste(x1,x2,sep="_")]
-    myRow2[,tuple3:=paste(x3[1],x3[2],sep="_")]
+    myRow2[,set3:=paste(c(x1,x2), collapse = "|")]
+    myRow2[,set4:=paste(c(x3[1],x3[2]),collapse = "|")]
     
     myY1 = c(myX,x1,x3[1])
     myY2 = c(myX,x1,x3[2])
@@ -100,6 +131,7 @@ dumTab = foreach(i = 1:dim(allTuples)[1])%do%{
     
     myQuads <-paste(c(quad1,quad2,quad3,quad4), collapse = "|")
     myRow2[,allQuads := myQuads]
+    myRow2[,tuple := NULL]
     myRow2
   }
   
@@ -107,29 +139,31 @@ dumTab = foreach(i = 1:dim(allTuples)[1])%do%{
   myRow2
 }
 
-dum3 = rbindlist(dumTab)
-dum3
+tab2 = rbindlist(dumTab)
+head(tab2)
 
-# I want to rename! set1 set2 set3 set4 allQuads, then merge
+#' # Merge sets ####
+#' ***
+myTab_n6 = rbind(tab1,tab2)
+dim(myTab_n6)
 
-tab1 = copy(dum2)
-names(tab1) = gsub("taxa","set",names(tab1))
-setnames(tab1,"triple","set4")
+myTab_n6[,status := "uncovered"]
+myTab_n6[,count := 0]
 
-tab2 = copy(dum3)
-names(tab2)
-tab2[,tuple := NULL]
-names(tab2) = gsub("taxa","set",names(tab2))
-setnames(tab2,"tuple2","set3")
-setnames(tab2,"tuple3","set4")
+save(myTab_n6,file = "../partitions/partitions_n6.RData")
 
-myTab = rbind(tab1,tab2)
-
-# all 45 partitions have to be covered ...
-myTab[,status := "uncovered"]
-myTab[,count := 0]
-
+#' # Test ####
+#' ***
+#' I use the second example data set from my PhyloDecR package. 
+#' 
+fn2 = system.file("extdata", 
+                  "example_2_Decisive.txt", 
+                  package = "PhyloDecR")
+test2 = createInput(fn=fn2,sepSym = ",")
+dummy = test2$data
 dummy2 = dummy[status=="input",]
+
+myTab = copy(myTab_n6)
 
 for(i in 1:dim(dummy2)[1]){
   #i=1
@@ -142,3 +176,10 @@ for(i in 1:dim(dummy2)[1]){
 
 table(myTab$count)
 if(min(myTab$count>0)) message("phylogenetically decisive according to 4WPP")
+
+#' Okay, the test is positive. The data set is decisive according to 4WPP & Fischers Algorithm.  
+#' 
+#' # SessionInfo ####
+#' ***
+sessionInfo()
+message("\nTOTAL TIME : " ,round(difftime(Sys.time(),time0,units = "mins"),3)," minutes")
